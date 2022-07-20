c1c5d42a26844a6749e1c3d4a583893dd0897886
Precillieo <kolawoleprecious99@gmail.com>
2022-07-20 16:42:56 +0100

    added postgres db

--- database.py
+++ database.py
@@ -5,7 +5,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.asyncio import AsyncSession
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./waitlist.db"
+#SQLALCHEMY_DATABASE_URL = "sqlite:///./waitlist.db"
+SQLALCHEMY_DATABASE_URL= "postgres://uovyxjmplmasat:1288339d5826bd87fc58414d15d03ada76d0247c96bf34234e2f18fb4e87e51e@ec2-34-235-31-124.compute-1.amazonaws.com:5432/dgsokp4o8nq2v"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
