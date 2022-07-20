4c3abed7193c6cdc5cf78059d39c07486b79a529
Precillieo <kolawoleprecious99@gmail.com>
2022-07-20 17:06:37 +0100

    added postgres db

--- database.py
+++ database.py
@@ -9,7 +9,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 SQLALCHEMY_DATABASE_URL= "postgresql://uovyxjmplmasat:1288339d5826bd87fc58414d15d03ada76d0247c96bf34234e2f18fb4e87e51e@ec2-34-235-31-124.compute-1.amazonaws.com:5432/dgsokp4o8nq2v"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
